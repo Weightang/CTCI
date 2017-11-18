e6b176564f626b78cb52166d51493ee032ce9e79
tang <weigh_tang@163.com>
2017-11-18 21:45:14 +0800

    java的实现

---
+++ Ctci_java/Chapter1/Question.java
@@ -0,0 +1,9 @@
+
+public class Question {
+
+    public static void main(String[] args) {
+        System.out.println("cccc");
+        Question1_1.isUniqueChars("ffff");
+    }
+
+}
---
+++ Ctci_java/Chapter1/Question1_1.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * 实现一个算法，确定一个字符串的所有字符是否全都不同。假使不允许使用额外的数据结构，
+ */
+public class Question1_1{
+  
+    public static boolean isUniqueChars(String str){
+            if(str.length()>256) return false;
+            boolean[] char_set=new boolean[256];
+            for(int i =0; i<str.length();i++){
+                int val=str.charAt(i);
+                if(char_set[val]){
+                    return false;
+                }
+                char_set[val]=true;
+            }
+            return true;
+    }
+
+  
+
+
+}
