373d20b0d633f763c3a7bae63a063d7c8ac3a30c
tang <weigh_tang@163.com>
2017-11-19 17:13:45 +0800

    问题1

--- CtciJava/Question1.java
+++ CtciJava/Question1.java
@@ -3,10 +3,12 @@
 /**
  * 实现一个算法，确定一个字符串的所有字符是否全都不同。假使不允许使用额外的数据结构，
  */
-public class Question1_1{
+public class Question1{
   
     public static boolean isUniqueChars(String str){
-            if(str.length()>256) return false;
+            if(str.length()>256) {
+                return false;
+            }
             boolean[] char_set=new boolean[256];
             for(int i =0; i<str.length();i++){
                 int val=str.charAt(i);
@@ -19,6 +21,10 @@ public class Question1_1{
     }
 
   
-
+public static void main(String[] args) {
+    // System.out.println("ccc");
+    boolean b= isUniqueChars("hello");
+    System.out.println(b);
+}
 
 }
--- Ctci_java/Chapter1/Question.java
+++
@@ -1,9 +0,0 @@
-
-public class Question {
-
-    public static void main(String[] args) {
-        System.out.println("cccc");
-        Question1_1.isUniqueChars("ffff");
-    }
-
-}
